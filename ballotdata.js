73253db8a145ed28da9114e762370644c4bb1657
shanon-rose <62967478+shanon-rose@users.noreply.github.com>
2021-08-14 18:00:54 -0600

    Update ballotdata.js

--- ballotdata.js
+++ ballotdata.js
@@ -2,15 +2,15 @@ const ballotData = {
   provincial: [
     {
       text: 'A ballot to pick federal senators to represent the province in Ottawa.',
-      description: 'Note: Senators are not elected in Canada.'
+      description: 'Note: <a href="https://www.elections.ab.ca/elections/senate-referendum/">Senators are not elected in Canada</a>.'
     },
     {
       text: 'Do you want Alberta to adopt year-round Daylight Saving Time, which is summer hours, eliminating the need to change our clocks twice a year?',
-      description: 'According to the order-in-council approving the referendum on daylight saving time, this result will be legally binding.'
+      description: 'According to the <a href="https://www.qp.alberta.ca/documents/Orders/Orders_in_Council/2021/2021_242.pdf">order in council</a>, the Lieutenant Governor in Council has specified that this result will be legally binding.'
     },
     {
       text: 'Should section 36(2) of the Constitution Act, 1982 - Parliament and the government of Canada\'s commitment to the principle of making equalization payments - be removed from the constitution?',
-      description: 'Note: Alberta would need federal government approval for any change and seven of 10 provinces would have to agree to any constitutional amendments.'
+      description: 'Note: <a href="https://www.constitutionalstudies.ca/2021/08/can-a-provincial-referendum-trigger-a-legal-duty-to-enter-constitutional-negotiations-albertas-2021-equalization-referendum-and-the-reference-re-secession-of-quebec/">Alberta would need federal government approval for any change and seven of 10 provinces would have to agree to any constitutional amendments</a>.'
     },
   ],
   municipalities: {
